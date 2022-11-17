e562f9482942263b0d0499ccc3e026e597c1b105
Thiago Santos <tos.oliveira@gmail.com>
2022-11-17 07:30:30 -0300

    refactor: preparing auto lint

--- src/core/domain/client/airports-client.ts
+++ src/core/domain/client/airports-client.ts
@@ -2,7 +2,6 @@ import { Coordinates, Airport } from 'src/core/domain/model';
 
 export abstract class AirportsClient {
   abstract getNearestAirports(
-    request: Coordinates,
-    maxResults?: number,
+    request: Coordinates,    maxResults?: number,
   ): Promise<Airport[]>;
 }
